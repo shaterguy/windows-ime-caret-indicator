f2a154c92572de0e96ae49a02e1f3a16ab63bbed
SHatergUY <108107617+shaterguy@users.noreply.github.com>
2026-09-21 14:16:26 +0900

    Add Windows native interop

---
+++ src/WindowsImeCaretIndicator/Native.cs
@@ -0,0 +1,238 @@
+using System.Runtime.InteropServices;
+
+namespace WindowsImeCaretIndicator;
+
+internal static class Native
+{
+    internal const int ObjIdCaret = unchecked((int)0xFFFFFFF8);
+
+    internal const uint EventSystemForeground = 0x0003;
+    internal const uint EventObjectFocus = 0x8005;
+    internal const uint EventObjectLocationChange = 0x800B;
+    internal const uint WineventOutOfContext = 0x0000;
+    internal const uint WineventSkipOwnProcess = 0x0002;
+
+    internal const int WhKeyboardLl = 13;
+    internal const uint WmKeyDown = 0x0100;
+    internal const uint WmSysKeyDown = 0x0104;
+
+    internal const uint WmImeControl = 0x0283;
+    internal const nuint ImcGetConversionMode = 0x0001;
+    internal const nuint ImcGetOpenStatus = 0x0005;
+    internal const uint SmtoAbortIfHung = 0x0002;
+
+    internal const uint WsOverlappedWindow = 0x00CF0000;
+    internal const uint WsVisible = 0x10000000;
+    internal const uint WsChild = 0x40000000;
+    internal const uint WsTabStop = 0x00010000;
+    internal const uint WsBorder = 0x00800000;
+    internal const uint EsAutoHScroll = 0x0080;
+    internal const int CwUseDefault = unchecked((int)0x80000000);
+    internal const uint WmDestroy = 0x0002;
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct Point
+    {
+        internal int X;
+        internal int Y;
+
+        internal Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct Rect
+    {
+        internal int Left;
+        internal int Top;
+        internal int Right;
+        internal int Bottom;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct GuiThreadInfo
+    {
+        internal uint cbSize;
+        internal uint flags;
+        internal nint hwndActive;
+        internal nint hwndFocus;
+        internal nint hwndCapture;
+        internal nint hwndMenuOwner;
+        internal nint hwndMoveSize;
+        internal nint hwndCaret;
+        internal Rect rcCaret;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct Message
+    {
+        internal nint hwnd;
+        internal uint message;
+        internal nuint wParam;
+        internal nint lParam;
+        internal uint time;
+        internal Point pt;
+        internal uint lPrivate;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    internal struct WindowClass
+    {
+        internal uint style;
+        internal WindowProc lpfnWndProc;
+        internal int cbClsExtra;
+        internal int cbWndExtra;
+        internal nint hInstance;
+        internal nint hIcon;
+        internal nint hCursor;
+        internal nint hbrBackground;
+        internal string? lpszMenuName;
+        internal string lpszClassName;
+    }
+
+    internal delegate void WinEventDelegate(
+        nint hook,
+        uint eventType,
+        nint hwnd,
+        int idObject,
+        int idChild,
+        uint eventThread,
+        uint eventTime);
+
+    internal delegate nint LowLevelKeyboardProc(
+        int nCode,
+        nuint wParam,
+        nint lParam);
+
+    internal delegate nint WindowProc(
+        nint hwnd,
+        uint message,
+        nuint wParam,
+        nint lParam);
+
+    [DllImport("user32.dll")]
+    internal static extern nint GetForegroundWindow();
+
+    [DllImport("user32.dll")]
+    internal static extern uint GetWindowThreadProcessId(nint hWnd, out uint processId);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool GetGUIThreadInfo(uint idThread, ref GuiThreadInfo info);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool ClientToScreen(nint hWnd, ref Point point);
+
+    [DllImport("user32.dll")]
+    internal static extern nint GetKeyboardLayout(uint idThread);
+
+    [DllImport("user32.dll")]
+    internal static extern uint GetDpiForWindow(nint hwnd);
+
+    [DllImport("imm32.dll")]
+    internal static extern nint ImmGetContext(nint hWnd);
+
+    [DllImport("imm32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool ImmReleaseContext(nint hWnd, nint hImc);
+
+    [DllImport("imm32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool ImmGetOpenStatus(nint hImc);
+
+    [DllImport("imm32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool ImmGetConversionStatus(nint hImc, out uint conversion, out uint sentence);
+
+    [DllImport("imm32.dll")]
+    internal static extern nint ImmGetDefaultIMEWnd(nint hWnd);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    internal static extern nint SendMessageTimeoutW(
+        nint hWnd,
+        uint msg,
+        nuint wParam,
+        nint lParam,
+        uint flags,
+        uint timeout,
+        out nint result);
+
+    [DllImport("user32.dll")]
+    internal static extern nint SetWinEventHook(
+        uint eventMin,
+        uint eventMax,
+        nint module,
+        WinEventDelegate callback,
+        uint process,
+        uint thread,
+        uint flags);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool UnhookWinEvent(nint hook);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    internal static extern nint SetWindowsHookExW(
+        int idHook,
+        LowLevelKeyboardProc callback,
+        nint module,
+        uint threadId);
+
+    [DllImport("user32.dll")]
+    internal static extern nint CallNextHookEx(nint hook, int code, nuint wParam, nint lParam);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool UnhookWindowsHookEx(nint hook);
+
+    [DllImport("user32.dll")]
+    internal static extern int GetMessageW(out Message message, nint hWnd, uint min, uint max);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool TranslateMessage(ref Message message);
+
+    [DllImport("user32.dll")]
+    internal static extern nint DispatchMessageW(ref Message message);
+
+    [DllImport("user32.dll")]
+    internal static extern void PostQuitMessage(int exitCode);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    internal static extern ushort RegisterClassW(ref WindowClass windowClass);
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    internal static extern nint CreateWindowExW(
+        uint exStyle,
+        string className,
+        string windowName,
+        uint style,
+        int x,
+        int y,
+        int width,
+        int height,
+        nint parent,
+        nint menu,
+        nint instance,
+        nint param);
+
+    [DllImport("user32.dll")]
+    internal static extern nint DefWindowProcW(nint hWnd, uint message, nuint wParam, nint lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+    internal static extern nint GetModuleHandleW(string? moduleName);
+
+    [DllImport("user32.dll")]
+    internal static extern nint LoadCursorW(nint instance, nint cursorName);
+
+    [DllImport("user32.dll")]
+    internal static extern nint SetFocus(nint hWnd);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool SetProcessDpiAwarenessContext(nint value);
+}
